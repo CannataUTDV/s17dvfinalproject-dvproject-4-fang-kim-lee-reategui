3953de4678228b44c9417291b787d45b0b436a0c
Orlando Reategui <orlandordiaz@gmail.com>
2017-05-01 23:26:43 -0500

    no message

---
+++ 02 Shiny/01 boxplot 02 histogram 03 scatterplot/server.R
@@ -0,0 +1,180 @@
+# server.R
+require(quantreg)
+require(ggplot2)
+require(dplyr)
+require(shiny)
+require(shinydashboard)
+require(data.world)
+require(readr)
+require(DT)
+require(leaflet)
+require(plotly)
+require(lubridate)
+
+
+online0 = TRUE
+
+# The following query is for the select list in the Boxplots -> Simple Boxplot tab, and Barcharts -> Barchart with Table Calculation tab.
+if(online0) {
+  STABBRs = query(
+    data.world(propsfile = "www/.data.world"),
+    dataset="cannata/superstoreorders", type="sql",
+    query="select distinct STABBR as D, STABBR as R
+    from SuperStoreOrders
+    order by 1"
+  ) # %>% View()
+} else {
+  print("Getting STABBRs from csv")
+  file_path = "www/CollegeScorecard.csv"
+  df <- readr::read_csv(file_path) 
+  tdf1 = df %>% dplyr::distinct(STABBR) %>% arrange(STABBR) %>% dplyr::rename(D = STABBR)
+  tdf2 = df %>% dplyr::distinct(STABBR) %>% arrange(STABBR) %>% dplyr::rename(R = STABBR)
+  STABBRs = bind_cols(tdf1, tdf2)
+}
+STABBR_list <- as.list(STABBRs$D, STABBRs$R)
+STABBR_list <- append(list("All" = "All"), STABBR_list)
+STABBR_list5 <- STABBR_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################### Start shinyServer Function ####################
+
+shinyServer(function(input, output) {   
+  # These widgets are for the Box Plots tab.
+  online5 = reactive({input$rb5})
+  output$boxplotSTABBRs <- renderUI({selectInput("selectedBoxplotSTABBRs", "Choose STABBRs:",
+                                                 STABBR_list5, multiple = TRUE, selected='All') })
+  
+  # These widgets are for the Histogram tab.
+  online4 = reactive({input$rb4})
+  
+  # These widgets are for the Scatter Plots tab.
+  online3 = reactive({input$rb3})
+  
+  
+  # Begin Box Plot Tab ------------------------------------------------------------------
+  dfbp1 <- eventReactive(input$click5, {
+    if(input$selectedBoxplotSTABBRs == 'All') STABBR_list5 <- input$selectedBoxplotSTABBRs
+    else STABBR_list5 <- append(list("Skip" = "Skip"), input$selectedBoxplotSTABBRs)
+    if(online5() == "SQL") {
+      print("Getting from data.world")
+      df <- query(
+        data.world(propsfile = "www/.data.world"),
+        dataset="cannata/superstoreorders", type="sql",
+        query="select Category, CONTROL, STABBR, CCBASIC
+        from SuperStoreOrders
+        where (? = 'All' or STABBR in (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?))",
+        queryParameters = STABBR_list5 ) # %>% View()
+    }
+    else {
+      print("Getting from csv")
+      file_path = "www/CollegeScorecard.csv"
+      df <- readr::read_csv(file_path)
+      #df %>% dplyr::select(Category, CONTROL, STABBR, CCBASIC) %>% dplyr::filter(STABBR %in% input$selectedBoxplotSTABBRs | input$selectedBoxplotSTABBRs == "All") # %>% View()
+    }
+  })
+  
+  output$boxplotData1 <- renderDataTable({DT::datatable(dfbp1(), rownames = FALSE,
+                                                        extensions = list(Responsive = TRUE, 
+                                                                          FixedHeader = TRUE)
+  )
+  })
+  
+  # dfbp2 <- eventReactive(c(input$click5, input$boxCONTROLRange1), {
+  #   dfbp1() %>% dplyr::filter(CONTROL >= input$boxCONTROLRange1[1] & CONTROL <= input$boxCONTROLRange1[2]) # %>% View()
+  # })
+  # 
+  # dfbp3 <- eventReactive(c(input$click5, input$range5a), {
+  #   dfbp2() %>% dplyr::filter(lubridate::year(CCBASIC) == as.integer(input$range5a) & lubridate::quarter(CCBASIC) == (4 * (input$range5a - as.integer(input$range5a))) + 1) %>% dplyr::arrange(desc(CCBASIC)) # %>% View()
+  # })
+  
+  output$boxplotPlot1 <- renderPlotly({
+    #View(dfbp3())
+    p <- ggplot(dfbp1()) + 
+      geom_boxplot(aes(x=CCBASIC, y=COSTT4_A, colour=factor(CONTROL))) + 
+      #ylim(0, input$boxCONTROLRange1[2]) +
+      theme(axis.text.x=element_text(angle=90, size=10, vjust=0.5)) +
+      labs(title="Average Cost of Attendance for Type of School Boxplot") + 
+      labs(color="Control")
+    ggplotly(p)
+  })
+  # End Box Plot Tab ___________________________________________________________
+  
+  # Begin Histogram Tab ------------------------------------------------------------------
+  dfh1 <- eventReactive(input$click4, {
+    if(online4() == "SQL") {
+      print("Getting from data.world")
+      query(
+        data.world(propsfile = "www/.data.world"),
+        dataset="cannata/superstoreorders", type="sql",
+        query="select Shipping_Cost, Container
+        from SuperStoreOrders
+        where Container = 'Small Box'"
+      ) # %>% View()
+    }
+    else {
+      print("Getting from csv")
+      file_path = "www/CollegeScorecard.csv"
+      df <- readr::read_csv(file_path)
+     # df %>% dplyr::select(Shipping_Cost, Container) %>% dplyr::filter(Container == 'Small Box') # %>% View()
+    }
+    })
+  
+  output$histogramData1 <- renderDataTable({DT::datatable(dfh1(), rownames = FALSE,
+                                                  extensions = list(Responsive = TRUE, 
+                                                  FixedHeader = TRUE)
+  )
+  })
+  
+  output$histogramPlot1 <- renderPlotly({p <- ggplot(dfh1()) +
+      geom_histogram(aes(x=SAT_AVG), alpha=1) +
+      theme(axis.text.x=element_text(angle=90, size=10, vjust=0.5))
+      #geom_line(aes(y = ..count.., color="red")) +
+      #geom_line(aes(fill=..count..), stat="bin", binwidth=10))
+      ggplotly(p)
+  })
+  # End Histogram Tab ___________________________________________________________
+  
+  # Begin Scatter Plots Tab ------------------------------------------------------------------
+  dfsc1 <- eventReactive(input$click3, {
+    if(online3() == "SQL") {
+      print("Getting from data.world")
+      query(
+        data.world(propsfile = "www/.data.world"),
+        dataset="cannata/superstoreorders", type="sql",
+        query="select CONTROL, Profit, State
+        from SuperStoreOrders
+        where State = 'Texas' or State = 'Florida'"
+      ) # %>% View()
+    }
+    else {
+      print("Getting from csv")
+      file_path = "www/CollegeScorecard.csv"
+      df <- readr::read_csv(file_path)
+      #df %>% dplyr::select(CONTROL, Profit, State) %>% dplyr::filter(State == 'Texas' | State == 'Florida') # %>% View()
+    }
+  })
+  output$scatterData1 <- renderDataTable({DT::datatable(dfsc1(), rownames = FALSE,
+                                                 extensions = list(Responsive = TRUE, 
+                                                 FixedHeader = TRUE)
+  )
+  })
+  output$scatterPlot1 <- renderPlotly({p <- ggplot(dfsc1()) + 
+
+      geom_point(aes(x=INEXPFTE, y=TUITFTE, colour=factor(STABBR)), size=2) +
+      theme(axis.text.x=element_text(angle=90, size=8, vjust=0.5)) + 
+      theme(axis.text.y=element_text(size=8, hjust=0.5)) 
+      ggplotly(p)
+  })
+  # End Scatter Plots Tab ___________________________________________________________
+})
---
+++ 02 Shiny/01 boxplot 02 histogram 03 scatterplot/ui.R
@@ -0,0 +1,85 @@
+#ui.R
+require(shiny)
+require(shinydashboard)
+require(DT)
+require(leaflet)
+require(plotly)
+
+dashboardPage(
+  dashboardHeader(
+  ),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Box Plots", tabName = "boxplot", icon = icon("dashboard")),
+      menuItem("Histograms", tabName = "histogram", icon = icon("dashboard")),
+      menuItem("Scatter Plots", tabName = "scatter", icon = icon("dashboard"))
+    )
+  ),
+  dashboardBody(
+    tags$style(type="text/css",
+               ".shiny-output-error { visibility: hidden; }",
+               ".shiny-output-error:before { visibility: hidden; }"
+    ),    
+    tabItems(
+      # Begin Box Plots tab content.
+      tabItem(tabName = "boxplot",
+              tabsetPanel(
+                tabPanel("Data",  
+                         radioButtons("rb5", "Get data from:",
+                                      c("SQL" = "SQL",
+                                        "CSV" = "CSV"), inline=T),
+                         uiOutput("boxplotSTABBRs"), # See http://shiny.rstudio.com/gallery/dynamic-ui.html,
+                         actionButton(inputId = "click5",  label = "To get data, click here"),
+                         hr(), # Add space after button.
+                         DT::dataTableOutput("boxplotData1")
+                ),
+                tabPanel("Simple Box Plot", 
+                         sliderInput("boxCONTROLRange1", "CONTROL Range:", # See https://shiny.rstudio.com/articles/sliders.html
+                                     min = min(globals$CONTROL), max = max(globals$CONTROL), 
+                                     value = c(min(globals$CONTROL), max(globals$CONTROL))),
+                         sliderInput("range5a", "Looping Animation:", 
+                                     min(globals$Order_Date), 
+                                     max(globals$Order_Date) + .75, 
+                                     max(globals$Order_Date), 
+                                     step = 0.25,
+                                     animate=animationOptions(interval=2000, loop=T)),
+                         plotlyOutput("boxplotPlot1", height=500))
+              )
+      ),
+      # End Box Plots tab content.
+      # Begin Histogram tab content.
+      tabItem(tabName = "histogram",
+              tabsetPanel(
+                tabPanel("Data",  
+                         radioButtons("rb4", "Get data from:",
+                                      c("SQL" = "SQL",
+                                        "CSV" = "CSV"), inline=T),
+                         actionButton(inputId = "click4",  label = "To get data, click here"),
+                         hr(), # Add space after button.
+                         DT::dataTableOutput("histogramData1")
+                ),
+                tabPanel("Simple Histogram", plotlyOutput("histogramPlot1", height=1000))
+              )
+      ),
+      # End Histograms tab content.
+      # Begin Scatter Plots tab content.
+      tabItem(tabName = "scatter",
+              tabsetPanel(
+                tabPanel("Data",  
+                         radioButtons("rb3", "Get data from:",
+                                      c("SQL" = "SQL",
+                                        "CSV" = "CSV"), inline=T),
+                         uiOutput("scatterStates"), # See http://shiny.rstudio.com/gallery/dynamic-ui.html,
+                         actionButton(inputId = "click3",  label = "To get data, click here"),
+                         hr(), # Add space after button.
+                         DT::dataTableOutput("scatterData1")
+                ),
+                tabPanel("Simple Scatter Plot", plotlyOutput("scatterPlot1", height=1000))
+              )
+      )
+      # End Scatter Plots tab content.
+
+    )
+  )
+)
+
