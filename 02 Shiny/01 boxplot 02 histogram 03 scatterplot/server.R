5adbdbb7cc07eaa109f35d834a0aad4ecf4127fd
Orlando Reategui <orlandordiaz@gmail.com>
2017-05-02 07:42:07 -0500

    shiny actions, interactivity, global.R, updated vis

--- 02 Shiny/01 boxplot 02 histogram 03 scatterplot/server.R
+++ 02 Shiny/01 boxplot 02 histogram 03 scatterplot/server.R
@@ -14,19 +14,23 @@ require(lubridate)
 online0 = TRUE
 
 # The following query is for the select list in the Boxplots -> Simple Boxplot tab, and Barcharts -> Barchart with Table Calculation tab.
-if(online0) {
-  STABBRs = query(
-    data.world(propsfile = "www/.data.world"),
-    dataset="cannata/superstoreorders", type="sql",
-    query="select distinct STABBR as D, STABBR as R
-    from SuperStoreOrders
-    order by 1"
-  ) # %>% View()
-} else {
-  print("Getting STABBRs from csv")
-  file_path = "www/CollegeScorecard.csv"
-  df <- readr::read_csv(file_path) 
-}
+# if(online0) {
+#   STABBRs = query(
+#     data.world(propsfile = "www/.data.world"),
+#     dataset="jlee/s-17-dv-final-project", type="sql",
+#     query="select distinct STABBR as D, STABBR as R
+#     from SuperStoreOrders
+#     order by 1"
+#   ) # %>% View()
+# } else {
+  # print("Getting STABBRs from csv")
+  # file_path = "www/CollegeScorecard.csv"
+  # df <- readr::read_csv(file_path) 
+# }
+  
+  #print("Getting STABBRs from csv")
+  #file_path = "www/CollegeScorecard.csv"
+  #df <- readr::read_csv(file_path) 
 
 ############################### Start shinyServer Function ####################
 shinyServer(function(input, output) {   
@@ -62,10 +66,38 @@ shinyServer(function(input, output) {
                                                                           FixedHeader = TRUE)
   )
   })
+  
+  gfbp1 <- eventReactive(input$SelectCost4a, {
+    if(online5() == "SQL") {
+      print("Getting from data.world")
+      df <- query(
+        data.world(propsfile = "www/.data.world"),
+        dataset="jlee/s-17-dv-final-project", type="sql",
+        query="select UNITID, CONTROL, STABBR, CCBASIC, COSTT4_A
+        from CollegeScorecard
+        where (COSTT4_A < ?)"
+        ,queryParameters = input$SelectCost4a
+      ) # %>% View()
+    }
+    else {
+      print("Getting from csv")
+      file_path = "www/CollegeScorecard.csv"
+      df <- readr::read_csv(file_path)
+      tdf <- df %>% dplyr::filter(COSTT4_A < input$SelectCost4a)
+    }
+    })
+  
+  
+  
+  
+  # You can access the value of the widget with input$select, e.g.
+  #output$costvalue <- renderPrint({ input$select })
+  
+
 
   output$boxplotPlot1 <- renderPlotly({
     #View(dfbp3())
-    p <- ggplot(dfbp1()) + 
+    p <- ggplot(gfbp1()) + 
       geom_boxplot(aes(x=CCBASIC, y=COSTT4_A, colour=factor(CONTROL))) + 
       #ylim(0, input$boxCONTROLRange1[2]) +
       theme(axis.text.x=element_text(angle=90, size=10, vjust=0.5)) +
@@ -82,7 +114,7 @@ shinyServer(function(input, output) {
       query(
         data.world(propsfile = "www/.data.world"),
         dataset="jlee/s-17-dv-final-project", type="sql",
-        query="select UNITID, SAT_AVG
+        query="select UNITID, SAT_AVG, STABBR
         from CollegeScorecard"
       ) # %>% View()
     }
@@ -90,20 +122,50 @@ shinyServer(function(input, output) {
       print("Getting from csv")
       file_path = "www/CollegeScorecard.csv"
       df <- readr::read_csv(file_path)
+      tdf <- df %>% dplyr::filter(STABBR == input$select)
      # df %>% dplyr::select(Shipping_Cost, Container) %>% dplyr::filter(Container == 'Small Box') # %>% View()
     }
     })
   
   output$histogramData1 <- renderDataTable({DT::datatable(dfh1(), rownames = FALSE,
-                                                  extensions = list(Responsive = TRUE, 
-                                                  FixedHeader = TRUE)
+                                                          extensions = list(Responsive = TRUE, 
+                                                                            FixedHeader = TRUE)
+                                                          
   )
   })
   
-  output$histogramPlot1 <- renderPlotly({p <- ggplot(dfh1()) +
-      geom_histogram(aes(x=SAT_AVG)) +
+  
+  gfh1 <- eventReactive(input$select, {
+    if(online4() == "SQL") {
+      print("Getting from data.world")
+      query(
+        data.world(propsfile = "www/.data.world"),
+        dataset="jlee/s-17-dv-final-project", type="sql",
+        query="select UNITID, SAT_AVG, STABBR, INSTNM
+        from CollegeScorecard
+        where (STABBR = ?)"
+        ,queryParameters = input$select
+      ) # %>% View()
+    }
+    else {
+      print("Getting from csv")
+      file_path = "www/CollegeScorecard.csv"
+      df <- readr::read_csv(file_path)
+      tdf <- df %>% dplyr::filter(STABBR == input$select)
+      # df %>% dplyr::select(Shipping_Cost, Container) %>% dplyr::filter(Container == 'Small Box') # %>% View()
+    }
+    })
+  
+  # You can access the value of the widget with input$select, e.g.
+  #output$value <- renderPrint({ input$select })
+  
+  
+  output$histogramPlot1 <- renderPlotly({p <- ggplot(gfh1()) +
+      geom_histogram(aes(x=SAT_AVG, color=INSTNM)) +
       theme(axis.text.x=element_text(angle=90, size=10, vjust=0.5)) + 
-      labs(title="AVERAGE SAT vs. Count Histogram")
+      labs(title="AVERAGE SAT vs. Count Histogram") +
+      labs(Color="Institution name/s") 
+      
       #geom_line(aes(y = ..count.., color="red")) +
       #geom_line(aes(fill=..count..), stat="bin", binwidth=10))
       ggplotly(p)
@@ -128,6 +190,8 @@ shinyServer(function(input, output) {
       #df %>% dplyr::select(CONTROL, Profit, State) %>% dplyr::filter(State == 'Texas' | State == 'Florida') # %>% View()
     }
   })
+  
+  
   output$scatterData1 <- renderDataTable({DT::datatable(dfsc1(), rownames = FALSE,
                                                  extensions = list(Responsive = TRUE, 
                                                  FixedHeader = TRUE)
--- 02 Shiny/01 boxplot 02 histogram 03 scatterplot/ui.R
+++ 02 Shiny/01 boxplot 02 histogram 03 scatterplot/ui.R
@@ -28,12 +28,20 @@ dashboardPage(
                          radioButtons("rb5", "Get data from:",
                                       c("SQL" = "SQL",
                                         "CSV" = "CSV"), inline=T),
-                         uiOutput("boxplotSTABBRs"), # See http://shiny.rstudio.com/gallery/dynamic-ui.html,
                          actionButton(inputId = "click5",  label = "To get data, click here"),
                          hr(), # Add space after button.
                          DT::dataTableOutput("boxplotData1")
                 ),
                 tabPanel("Simple Box Plot", 
+                         
+                         sliderInput("SelectCost4a", label = h4("Select Maximum cost of attendance"), 
+                                     min = min(df$COSTT4_A, na.rm = TRUE), max = max(df$COSTT4_A, na.rm = TRUE),  
+                                     value = max(df$COSTT4_A, na.rm = TRUE)),
+                         
+                         #hr(),
+                         #fluidRow(column(3, verbatimTextOutput("costvalue"))),
+                         
+                         
                          plotlyOutput("boxplotPlot1", height=500))
               )
       ),
@@ -42,14 +50,27 @@ dashboardPage(
       tabItem(tabName = "histogram",
               tabsetPanel(
                 tabPanel("Data",  
+                         
                          radioButtons("rb4", "Get data from:",
                                       c("SQL" = "SQL",
                                         "CSV" = "CSV"), inline=T),
                          actionButton(inputId = "click4",  label = "To get data, click here"),
+                         
+
                          hr(), # Add space after button.
                          DT::dataTableOutput("histogramData1")
                 ),
-                tabPanel("Simple Histogram", plotlyOutput("histogramPlot1", height=1000))
+                tabPanel("Simple Histogram",
+                         
+                         selectInput("select", label = h4("Select State"), 
+                                     choices = unique(df$STABBR), 
+                                     selected = 1),
+                         
+                         hr(),
+                         fluidRow(column(3, verbatimTextOutput("value"))),
+                         
+                         
+                         plotlyOutput("histogramPlot1", height=1000))
               )
       ),
       # End Histograms tab content.
