1a48bc6ade82b25d0560205e7bd4dbae4c0aa51c
ASUS\Franny <frannyfang@gmail.com>
2017-05-02 12:39:54 -0500

    no message

--- 02 Shiny/Shiny 02/ui.R
+++ 02 Shiny/Shiny 02/ui.R
@@ -7,12 +7,75 @@ dashboardPage(
   ),
   dashboardSidebar(
     sidebarMenu(
+      menuItem("Box Plots", tabName = "boxplot", icon = icon("dashboard")),
+      menuItem("Histograms", tabName = "histogram", icon = icon("dashboard")),
+      menuItem("Scatter Plots", tabName = "scatter", icon = icon("dashboard")),
       menuItem("Crosstabs 1", tabName = "crosstab1", icon = icon("dashboard")),
       menuItem("Crosstabs 2", tabName = "crosstab2", icon = icon("dashboard"))
     )
   ),
   dashboardBody(    
     tabItems(
+      # Begin Box Plots tab content.
+      tabItem(tabName = "boxplot",
+              tabsetPanel(
+                tabPanel("Data",  
+                         radioButtons("rb5", "Get data from:",
+                                      c("SQL" = "SQL",
+                                        "CSV" = "CSV"), inline=T),
+                         actionButton(inputId = "click5",  label = "To get data, click here"),
+                         hr(), # Add space after button.
+                         DT::dataTableOutput("boxplotData1")
+                ),
+                tabPanel("Simple Box Plot", 
+                         sliderInput("SelectCost4a", label = h4("Select Maximum cost of attendance"), 
+                                     min = min(df$COSTT4_A, na.rm = TRUE), max = max(df$COSTT4_A, na.rm = TRUE),  
+                                     value = max(df$COSTT4_A, na.rm = TRUE)),
+                         plotlyOutput("boxplotPlot1", height=500))
+              )
+      ),
+      # End Box Plots tab content.
+      # Begin Histogram tab content.
+      tabItem(tabName = "histogram",
+              tabsetPanel(
+                tabPanel("Data",  
+                         
+                         radioButtons("rb4", "Get data from:",
+                                      c("SQL" = "SQL",
+                                        "CSV" = "CSV"), inline=T),
+                         actionButton(inputId = "click4",  label = "To get data, click here"),
+                         hr(), # Add space after button.
+                         DT::dataTableOutput("histogramData1")
+                ),
+                tabPanel("Simple Histogram",
+                         
+                         selectInput("select", label = h4("Select State"), 
+                                     choices = unique(df$STABBR), 
+                                     selected = 1),
+                         hr(),
+                         fluidRow(column(3, verbatimTextOutput("value"))),
+                         
+                         
+                         plotlyOutput("histogramPlot1", height=1000))
+              )
+      ),
+      # End Histograms tab content.
+      # Begin Scatter Plots tab content.
+      tabItem(tabName = "scatter",
+              tabsetPanel(
+                tabPanel("Data",  
+                         radioButtons("rb3", "Get data from:",
+                                      c("SQL" = "SQL",
+                                        "CSV" = "CSV"), inline=T),
+                         uiOutput("scatterStates"), # See http://shiny.rstudio.com/gallery/dynamic-ui.html,
+                         actionButton(inputId = "click3",  label = "To get data, click here"),
+                         hr(), # Add space after button.
+                         DT::dataTableOutput("scatterData1")
+                ),
+                tabPanel("Simple Scatter Plot", plotlyOutput("scatterPlot1", height=1000))
+              )
+      ),
+      # End Scatter Plots tab content.
       # Begin Crosstab 1 tab content.
       tabItem(tabName = "crosstab1",
               tabsetPanel(
